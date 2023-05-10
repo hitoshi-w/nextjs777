789da0eb725745707802f71c18ec87cd8368539d
h_watanabe <h.watanabe@dptr.jp>
2023-05-10 23:05:27 +0900

    prisma client設定

---
+++ lib/prisma.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+const client =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query"],
+  });
+
+// 開発環境では、ホットリロードするたびにクライアントが作成されるのを防ぐ
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = client;
+
+export default client
